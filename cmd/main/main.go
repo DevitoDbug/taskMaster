fe7a541c4573581dc3e7d0d5d5da4620f1ac8bc9
devitoDbug <davidOchiengy@gmail.com>
2023-10-28 20:46:11 +0300

    feat: Serve HTML page at root endpoint

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/gorilla/mux"
+	"html/template"
 	"net/http"
 	"taskMaster/pkg/routes"
 )
@@ -11,8 +12,12 @@ func main() {
 	port := ":8000"
 
 	r := mux.NewRouter()
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		tmp := template.Must(template.ParseFiles("../../static/index.html"))
+		tmp.Execute(w, nil)
+	}).Methods("GET")
 	routes.RegisterRoutes(r)
 
 	fmt.Println("Starting sever at port ", port)
-	http.ListenAndServe(port, nil)
+	http.ListenAndServe(port, r)
 }
